3ba108787cb13d2f7794eefec3caaeee23f7609d
Shubham <shubhamsinghmau2@gmail.com>
2022-09-25 16:42:13 +0530

    While Loops in JavaScript

---
+++ 010.WhileLoops/index.js
@@ -0,0 +1,21 @@
+// while(condition){
+//   code
+// }
+
+let n= prompt("Enter the value of n");
+let num = Number.parseInt(n);
+
+let i = 0;
+while(i<num){
+  console.log(i);
+  i++;
+}
+
+// do{
+    // code
+// }while(condition)
+let number = 2;
+do{
+  console.log(i);
+  i++;
+}while(i<number)
