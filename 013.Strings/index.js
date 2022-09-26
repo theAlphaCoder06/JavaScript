2c913727d4ba3f674a79cd9ca129346ef12fc4d1
Shubham <shubhamsinghmau2@gmail.com>
2022-09-26 21:49:55 +0530

    Strings in JavaScript

---
+++ 013.Strings/index.js
@@ -0,0 +1,20 @@
+let name = "Shubham";
+let second_name = 'Sunny';
+console.log(name[0]);
+console.log(name.length);
+
+// Template Literal- use backticks to insert variables
+// We can insert variables directly in tempelate literal, this is called string interpolation
+
+let boy1 = "Sunny";
+let boy2 = "Shubham";
+
+let sentence = `${boy1} is a friend of ${boy2}`;
+console.log(sentence);
+
+// Escape sequence
+
+console.log("Shubham\'s repl");
+// \t --> tab
+// \r --> carriage return
+// \n --> new line
